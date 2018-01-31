f1a357c5e25c27739d437a1556b7e7ec646b1a92
Tito <rabelo51@gmail.com>
2018-01-31 15:43:46 -0300

    Template language and index.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import ListView, TemplateView
+from .models import Post
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(ListView):
 
     template_name="blog/index.html"
+    model = Post
+    context_object_name = "post_list"
 
 index = IndexView.as_view()

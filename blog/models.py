2b1f93891859a9be788f3e284aca1c0269bb7032
Tito <rabelo51@gmail.com>
2018-01-31 14:14:28 -0300

    Post model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+
+    # no need for author (because it's always myself)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_add_now=True)
+    date_posted = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.date_posted = timezone.now() 
+        self.save()
+
+# field types: https://docs.djangoproject.com/en/2.0/ref/models/fields/#field-types

02651e4488a3ef68950c98bdac360c14a4771f90
Tito <rabelo51@gmail.com>
2018-02-01 15:04:27 -0300

    PortuguesePostsView, urls and html

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ class IndexView(ListView):
     template_name="blog/index.html"
     model = Post
     context_object_name = "post_list"
+    # pagination?
 
 class PostView(DetailView):
     
@@ -15,5 +16,19 @@ class PostView(DetailView):
     model = Post
     context_object_name = "post"    
 
+class PortuguesePostsView(ListView):
+    
+    template_name = "blog/portuguese_posts.html"
+    context_object_name = "pt_br_posts"
+    # pagination?
+
+    def get_queryset(self):
+        
+        queryset = Post.objects.filter(language="portuguese-br") 
+
+        return queryset
+
+
 index = IndexView.as_view()
 post = PostView.as_view()
+pt_posts = PortuguesePostsView.as_view()

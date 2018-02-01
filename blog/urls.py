028bac5e32c83fa9904a0ddbe0de8432051f89b9
Tito <rabelo51@gmail.com>
2018-02-01 15:04:27 -0300

    PortuguesePostsView, urls and html

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from blog import views
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'articles/(?P<slug>[\w_-]+)?', views.post, name='post'),
+    path('pt-br', views.pt_posts, name='pt-br'),
 ]
 
 # r'^articles/<str:date_published>/(?P<slug>[\w_-]+)?'

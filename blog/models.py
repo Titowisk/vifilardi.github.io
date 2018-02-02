3cdb49ced8c453c20c93c62cbde41500c6b4be38
Tito <rabelo51@gmail.com>
2018-02-02 11:45:26 -0300

    Tags models: ManyToMany

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,12 @@ class Post(models.Model):
     slug = models.SlugField(max_length=100, unique=True, null=True) # remove null atributte when erasing the database
     text = models.TextField()
     language = models.CharField(max_length=50, default='portuguese-br', blank=True) # portuguese, english, etc..
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_posted = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    posted = models.DateTimeField(blank=True, null=True)
+    #modified?
+    tag = models.ManyToManyField(
+        'Tags', related_name='posts', null=True, blank=True, on_delete=models.CASCADE
+        )
 
     def __str__(self):
         return self.title
@@ -19,5 +23,14 @@ class Post(models.Model):
     def publish(self):
         self.date_posted = timezone.now() 
         self.save()
+
+class Tags(models.Model):
+
+    tag = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
 
 # field types: https://docs.djangoproject.com/en/2.0/ref/models/fields/#field-types

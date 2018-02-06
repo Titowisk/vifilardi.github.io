de226777bc02f39e4118b180e891d7063917c5cb
Tito <rabelo51@gmail.com>
2018-02-06 15:33:29 -0300

    heroku config

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+# heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Heroku
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,3 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# heroku config
+
+    # Change 'default' database configuration with $DATABASE_URL.
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')

ce215ed5abe14825f9f5c8e557f575a903806b9c
Vitor Filardi <rabelo51@gmail.com>
2019-09-21 15:52:16 -0300

    Update SECRET_KEY management

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e)v@n-g9hfez7+xkto2e_f(_9m8g2rt3z#-lek)w*kd2otlgyq'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
